42491233bbc039c4bb0b9341bbcc8ec264105965
Ferbit04 <learningcodeprogress@gmail.com>
2021-11-09 02:38:52 +0000

    adding path, controller, routes, and modifying a lot of files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+//ROUTES
+app.use(require('./routes/index.routes'))
+
+//STATIC FILES
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/404.html'))
+});
+
+app.listen(port, ()=>{
+    console.log(`Corriendo en el puerto ${port}`)
+});
---
+++ src/controller/index.controller.js
@@ -0,0 +1,6 @@
+const controller = {};
+controller.index = (req,res)=>{
+    res.send('Todo bien')
+};
+
+module.exports = controller
